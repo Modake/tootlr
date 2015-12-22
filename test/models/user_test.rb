63d6f62b23c15f8aee03d410ee24ee982f191cca
Chris Morse <Chris@Modake.com>
2015-12-22 17:11:38 -0500

    finish basic user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 	
 	def setup
-		@user = User.new(name: "Example", email: "user@example.com")
+		@user = User.new(name: "Example", email: "user@example.com",
+			password: "foobar", password_confirmation: "foobar")
 	end
 	
 	test "should be valid" do
@@ -17,6 +18,11 @@ class UserTest < ActiveSupport::TestCase
 
 	test "name should not be too long" do
 		@user.name = "a" * 51
+		assert_not @user.valid?
+	end
+	
+	test "password should have a minimum length" do
+		@user.password = @user.password_confirmation = "a" * 5
 		assert_not @user.valid?
 	end
 	
