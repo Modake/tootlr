34f1ccc7bd0361836b777f1a11e7a71da4d9cb07
Chris Morse <Chris@Modake.com>
2015-12-31 09:06:15 -0500

    Create account activation tests.

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20160101144206) do
     t.string   "password_digest"
     t.string   "remember_digest"
     t.boolean  "admin",             default: false
-    t.string   "actication_digest"
+    t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
   end
