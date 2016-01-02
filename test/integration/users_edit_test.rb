969f240b1f721a5e5edde3338efc5b7992f5a2b4
Chris Morse <Chris@Modake.com>
2016-01-02 10:23:25 -0500

    apply test fixes

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -1,24 +1,25 @@
 require 'test_helper'
 
 class UsersEditTest < ActionDispatch::IntegrationTest
-	def setup
-		@user = users(:michael)
-	end
-	
-	test "unsuccessful edit" do
-		log_in_as(@user)
-		get edit_user_path(@user)
-		assert_template 'users/edit'
-		patch user_path(@user), user: { name: "",
-																	  email: "user@invalid",
-																		password: "foo",
-																		password_confirmation: "bar" }
-		assert_template 'users/edit'
-	end
-	
-	test "successful edit with friendly forwarding" do
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "unsuccessful edit" do
     log_in_as(@user)
-		get edit_user_path(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), user: { name: "",
+                                    email: "user@invalid",
+                                    password:              "foo",
+                                    password_confirmation: "bar" }
+    assert_template 'users/edit'
+  end
+
+  test "successful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    log_in_as(@user)
     assert_redirected_to edit_user_path(@user)
     name  = "Foo Bar"
     email = "foo@bar.com"
